ddf47a7dd9ea24d0a9fa58be7aa0d009b4c4e796
aleksey <aleksey_tsoy@epam.com>
2018-07-09 23:49:19 +0600

    upd security config

--- src/main/java/com/flowershop/config/SecurityConfiguration.java
+++ src/main/java/com/flowershop/config/SecurityConfiguration.java
@@ -1,19 +1,25 @@
 package com.flowershop.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
+    @Qualifier("userServiceImpl")
     private UserDetailsService userDetailsService;
 
     @Override
@@ -23,9 +29,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and().httpBasic()
+                .and().csrf().disable();
     }
 
-    private BCryptPasswordEncoder bCryptPasswordEncoder() {
+    @Bean
+    public PasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder(11);
     }
 }
--- src/main/java/com/flowershop/service/impl/UserServiceImpl.java
+++ src/main/java/com/flowershop/service/impl/UserServiceImpl.java
@@ -6,6 +6,7 @@ import com.flowershop.domain.User;
 import com.flowershop.repository.BasketRepository;
 import com.flowershop.repository.UserRepository;
 import com.flowershop.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -17,14 +18,11 @@ import java.util.Optional;
 @Service
 public class UserServiceImpl implements UserService {
 
-    private final UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-    private final BasketRepository basketRepository;
-
-    public UserServiceImpl(UserRepository userRepository, BasketRepository basketRepository) {
-        this.userRepository = userRepository;
-        this.basketRepository = basketRepository;
-    }
+    @Autowired
+    private BasketRepository basketRepository;
 
     @Override
     public Collection<User> findAllUsers() {
